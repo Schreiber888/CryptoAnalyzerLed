6ef93135aa15546d4c4a447a5797927212dd67a2
Bekk I.V. <888_1@mail.ru>
2025-03-04 20:26:05 +0500

    add test

--- src/main/java/com/javarush/bekk/CaesarCipher.java
+++ src/main/java/com/javarush/bekk/CaesarCipher.java
@@ -1,6 +1,8 @@
 package com.javarush.bekk;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 public class CaesarCipher {
@@ -8,24 +10,19 @@ public class CaesarCipher {
             'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
             'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
 
-    public void encrypt(String inputFile, String outputFile, int key) {
+    public void encrypt(String inputFile, String outputFile, int key) throws IOException {
         // Реализация шифрования
         char[] arrayAlphabet = new char[ALPHABET.length];
-        for (int i = 0, j = 0; i < ALPHABET.length; i++, j++) {
+        for (int i = 0, j = 0, k = 0; i < ALPHABET.length; i++, j++) {
             if (j < ALPHABET.length - key) {
                 arrayAlphabet[i] = ALPHABET[j + key]; //key сделать ограничение
             } else {
-                j = 0;
-                arrayAlphabet[i] = ALPHABET[j];
+                arrayAlphabet[i] = ALPHABET[k];
+                k++;
             }
         }
-        try (FileReader reader = new FileReader(inputFile);
-             FileWriter writer = new FileWriter(outputFile)) {
-            //tringTokenizer stringTokenizer = new StringTokenizer()
-            /*while (reader.ready()){
-                char symbol = (char) reader.read();
-                writer.write(symbol);
-            }*/
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(inputFile));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(outputFile))) {
             int value;
             while ((value = reader.read()) > -1) {
                 char symbol = (char) value;
@@ -34,18 +31,51 @@ public class CaesarCipher {
                     if (symbolToLower == ALPHABET[i]) {
                         //System.out.print(arrayAlphabet[i]);
                         writer.write(arrayAlphabet[i]);
-                    } else if (symbolToLower == '\n') {
+
+                    } else if (symbol == '\n') {
                         writer.write('\n');
+                        i = ALPHABET.length;
                     }
                 }
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-        /*public void decrypt (String inputFile, String outputFile,int key){
-            // Реализация расшифровки
-        }*/
+    public void decrypt(String inputFile, String outputFile, int key) {
+        // Реализация расшифровки
+        char[] arrayAlphabet = new char[ALPHABET.length];
+        for (int i = 0, j = 0, k = 0; i < ALPHABET.length; i++, j++) {
+            if (j < ALPHABET.length - key) {
+                arrayAlphabet[i] = ALPHABET[j + key]; //key сделать ограничение
+            } else {
+                arrayAlphabet[i] = ALPHABET[k];
+                k++;
+            }
+        }
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(inputFile));
+             BufferedWriter writer = Files.newBufferedWriter(Path.of(outputFile))) {
+            int value;
+            while ((value = reader.read()) > -1) {
+                char symbol = (char) value;
+                for (int i = 0; i < ALPHABET.length; i++) {
+                    char symbolToLower = Character.toLowerCase(symbol);
+                    if (symbolToLower == arrayAlphabet[i]) {
+                        //System.out.print(arrayAlphabet[i]);
+                        writer.write(ALPHABET[i]);
+
+                    } else if (symbol == '\n') {
+                        writer.write('\n');
+                        i = arrayAlphabet.length;
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
--- src/main/java/com/javarush/bekk/MainApp.java
+++ src/main/java/com/javarush/bekk/MainApp.java
@@ -1,14 +1,17 @@
 package com.javarush.bekk;
 
 
+import java.io.IOException;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CaesarCipher caesarCipher = new CaesarCipher();
         String inputText = "I:\\j\\project1\\text.txt";
-        String outputText = "I:\\j\\project1\\encryptText.txt";
-        int key = 2;
-        caesarCipher.encrypt(inputText, outputText, key);
+        String outputTextEncrypt = "I:\\j\\project1\\encryptText.txt";
+        String outputTextDecrypt = "I:\\j\\project1\\decryptText.txt";
+        int key = 5;
+        caesarCipher.encrypt(inputText, outputTextEncrypt, key);
+        caesarCipher.decrypt(outputTextEncrypt, outputTextDecrypt, key);
     }
 
 
